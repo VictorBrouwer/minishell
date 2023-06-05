05cfd8b746d5b84ba103f7373a44169cea48d17a
Martin Haan <mhaan@f1r3s9.codam.nl>
2023-06-05 18:45:30 +0200

    made env

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -1,2 +1,45 @@
 #include "libft.h"
 #include "shell.h"
+
+typedef struct s_env_list
+{
+    char                *name;
+    char                *content;
+    struct s_env_list   *next;
+}   t_env_list;
+
+int    ft_env(t_env_list *env)
+{
+    t_env_list  *ptr;
+
+    ptr = env;
+    while (ptr)
+    {
+        if (ptr->name && ptr->content)
+        {
+            if(ft_putstr_fd_protected(ptr->name, STDOUT_FILENO, 0) == -1)
+                return (1);
+            if(ft_putstr_fd_protected("=", STDOUT_FILENO, 0) == -1)
+                return (1);
+            if(ft_putstr_fd_protected(ptr->content, STDOUT_FILENO, 1) == -1)
+                return (1);
+        }
+        ptr = ptr->next;
+    }
+    return (0);
+}
+
+// int main(void)
+// {
+//     t_env_list  *env;
+//
+//     env = malloc(sizeof(t_env_list));
+//     env->name = "",
+//     env->content = "test",
+//     env->next = malloc(sizeof(t_env_list));
+//     env->next->name = "PWD";
+//     env->next->content = "/Users/username";
+//     env->next->next = NULL;
+//     ft_printf("%d\n", ft_env(env));
+//     exit(0);
+// }
