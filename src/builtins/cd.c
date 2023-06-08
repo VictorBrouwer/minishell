0a35ce4be2e30fcea783e9296faad4d028df815a
Martin Haan <mhaan@f1r3s7.codam.nl>
2023-06-08 18:29:20 +0200

    made env list funcs

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -1,58 +1,58 @@
 #include "libft.h"
 #include "shell.h"
 
-static char	*get_path(t_command *cmd, char *cwd, t_minishell *shell);
+static char *get_path(t_command *cmd, char *cwd, t_minishell *shell);
 
-int	ft_cd(t_command *cmd, t_shell *shell)
-{
-	char	*cwd;
-	char	*oldpwd;
-	char	*path;
+int ft_cd(t_command *cmd, t_shell *shell) {
+  char *cwd;
+  char *oldpwd;
+  char *path;
 
-    cwd = getcwd(NULL, 0);
-    if (!cwd)
-        return (-1);
-	path = get_path(cmd, cwd, shell);
-	if (!path)
-		return (free(cwd), -1);
-    if (chdir(path) != 0)
-        return (free(cwd), -1);
-    oldpwd = ft_strdup(cwd);
-	free(cwd);
-    cwd = getcwd(NULL, 0);
-    if (!cwd)
-        return (-1);
-    return (0);
+  cwd = getcwd(NULL, 0);
+  if (!cwd)
+    return (-1);
+  path = get_path(cmd, cwd, shell);
+  if (!path)
+    return (free(cwd), -1);
+  if (chdir(path) != 0)
+    return (free(cwd), -1);
+  oldpwd = ft_strdup(cwd);
+  free(cwd);
+  cwd = getcwd(NULL, 0);
+  if (!cwd)
+    return (-1);
+  return (0);
 }
 
-static char	*get_path(t_command *cmd, char *cwd, t_minishell *shell)
-{
-	char	*path;
+static char *get_path(t_command *cmd, char *cwd, t_minishell *shell) {
+  char *path;
 
-	if (!cmd[1])
-        path = ft_strdup(shell->env->home);
-    else if (cmd[1] == ".")
-        path = ft_strdup(shell->env->pwd);
-    // else if (cmd[1] == "..")
-	// 	path = ;
-    else if (cmd[1] == "-")
-        path = ft_strdup(shell->env->oldpwd);
-    else if (ft_strncmp(cmd[1], "~/", 2) == 0)
-        path = ft_strjoin(shell->env->home, cmd[1] + 1);
-    else if (cmd[1] == "~")
-        path = ft_strdup(shell->env->home);
-    else if (cmd[1][0] == '/')
-        path = ft_strdup(cmd[1]);
-	else
-		path = ft_strdup(cmd[1]);
-	return (path);
+  if (!cmd[1])
+    path = ft_strdup(shell->env->home);
+  else if (cmd[1] == ".")
+    path = ft_strdup(shell->env->pwd);
+  // else if (cmd[1] == "..")
+  // 	path = ;
+  else if (cmd[1] == "-")
+    path = ft_strdup(shell->env->oldpwd);
+  else if (ft_strncmp(cmd[1], "~/", 2) == 0)
+    path = ft_strjoin(shell->env->home, cmd[1] + 1);
+  else if (cmd[1] == "~")
+    path = ft_strdup(shell->env->home);
+  else if (cmd[1][0] == '/')
+    path = ft_strdup(cmd[1]);
+  else
+    path = ft_strdup(cmd[1]);
+  return (path);
 }
 
 /*
 1. set new_working_dir:
 'cd'				: change cwd to "HOME"
-'cd -'				: change cwd to previous working directory (OLDPWD), ignore other arguments AND PRINT new cwd
-'cd .'				: change cwd to current working directory aka does nothing
+'cd -'				: change cwd to previous working directory
+(OLDPWD), ignore other arguments AND PRINT new cwd
+'cd .'				: change cwd to current working directory aka
+does nothing
 'cd ..'				: change cwd to directory 'above' cwd
 'cd ~'				: change cwd to "HOME" of current user
 'cd ~/path'			: change cwd to "HOME + path" of current user
@@ -63,7 +63,20 @@ static char	*get_path(t_command *cmd, char *cwd, t_minishell *shell)
 2. save current working directory into "OLDPWD=" in envp list
 
 3. change cwd to new working directory with chdir()
-chdir() = 0 indicates success: the operating system updates the process's current working directory
+chdir() = 0 indicates success: the operating system updates the process's
+current working directory
 
 4. save new working directory into "PWD=" in envp list
 */
+
+int main(int argc, char **argv) {
+  t_env_list *env_list;
+
+  if (argc != 4) {
+    ft_putstr_fd("Usage: ./a.out <home_path> <cwd_path> <owd_path>\n");
+    return (1);
+  }
+  env_list = init_env(argv[1], argv[2], argv[3]);
+  print_env_list(env_list);
+  return (0);
+}
--- src/env/env_list_funcs.c
+++ src/env/env_list_funcs.c
@@ -14,7 +14,7 @@ t_env_list	*new_env_var(char *name, char *content)
 	return (var);
 }
 
-void	ft_lstadd_back(t_env_list **lst, t_env_list *new)
+void	env_lstadd_back(t_env_list **lst, t_env_list *new)
 {
 	t_env_list	*lstptr;
 
@@ -29,4 +29,32 @@ void	ft_lstadd_back(t_env_list **lst, t_env_list *new)
 	while (lstptr->next)
 		lstptr = lstptr->next;
 	lstptr->next = new;
+}
+
+t_env_list	*init_env(char *home, char *cwd, char *owd)
+{
+	t_env_list	*env;
+	t_end_list	*home_var;
+	t_end_list	*cwd_var;
+	t_end_list	*owd_var;
+
+	home_var = new_env_var("home", home);
+	env = env_lstadd_back(env, home_var);
+	cwd_var = new_env_var("cwd", cwd);
+	env = env_lstadd_back(env, cwd_var);
+	owd_var = new_env_var("owd", owd);
+	env = env_lstadd_back(env, odw_var);
+	return (env);
+}
+
+void print_env_list(t_env_list *env)
+{
+	while (env)
+	{
+		ft_putstr_fd(env->name);
+		ft_putstr_fd("=");
+		ft_putstr_fd(env->content);
+		ft_putstr_fd("\n");
+		env = env->next;
+	}
 }
