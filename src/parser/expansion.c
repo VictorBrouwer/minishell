76328ffcdbee54cf78219a7a5551cf131f383ce6
Victor Brouwer <vbrouwer@f1r5s15.codam.nl>
2023-07-19 16:34:10 +0200

    fixed expansion bug

--- src/parser/expansion.c
+++ src/parser/expansion.c
@@ -1,6 +1,7 @@
 #include "shell.h"
 #include "libft.h"
 
+static void	check_and_expand_exit_status(t_token *token);
 // there can be nothing, quotes or an empty string after a env_var
 
 void	expand(t_token *top, t_shell *shell)
@@ -8,18 +9,37 @@ void	expand(t_token *top, t_shell *shell)
 	t_token	*curr;
 
 	curr = top;
-	if (strings_equal(top->content, "$?"))
-	{
-		free(top->content);
-		top->content = ft_itoa(glob_status);
-	}	
+
+	// if (ft_strncmp(top->content, "$?", 3) == 0)
+	// {
+	// 	free(top->content);
+	// 	top->content = ft_itoa(glob_status);
+	// }
+	check_and_expand_exit_status(top);
 	if (curr->token_id == ENV_VAR)
 		replace(curr, shell->env_list);
 	while (curr->next)
 	{
 		if (curr->next->token_id == ENV_VAR && curr->token_id != HEREDOC) // if env_var comes after a heredoc it should not be expanded
+		{
+			check_and_expand_exit_status(curr->next);
 			replace(curr->next, shell->env_list);
+		}
 		curr = curr->next;
+	}
+	// if (ft_strncmp(curr->content, "$?", 3) == 0)
+	// {
+	// 	free(curr->content);
+	// 	curr->content = ft_itoa(glob_status);
+	// }
+}
+
+void	check_and_expand_exit_status(t_token *token)
+{
+	if (ft_strncmp(token->content, "$?", 3) == 0)
+	{
+		free(token->content);
+		token->content = ft_itoa(glob_status);
 	}
 }
 
