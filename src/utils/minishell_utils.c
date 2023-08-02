b9281dea727c640240cba6ae2a2c51d24d0902ec
Victor Brouwer <vbrouwer@f1r3s16.codam.nl>
2023-08-02 13:24:21 +0200

    refactored more

--- src/utils/minishell_utils.c
+++ src/utils/minishell_utils.c
@@ -1,16 +1,16 @@
 #include	"shell.h"
 #include	"libft.h"
 
-bool strings_equal(const char *s1, const char *s2)
+bool	strings_equal(const char *s1, const char *s2)
 {
-    while (*s1 || *s2)
-    {
-        if (*s1 != *s2)
-            return (0);
-        s1++;
-        s2++;
-    }
-    return (1);
+	while (*s1 || *s2)
+	{
+		if (*s1 != *s2)
+			return (0);
+		s1++;
+		s2++;
+	}
+	return (1);
 }
 
 int	ft_isspace(int c)
@@ -26,9 +26,9 @@ void	update_status(pid_t pid)
 
 	status = 0;
 	waitpid(pid, &status, 0);
-	if (WIFEXITED(status)) // returns true if child was not terminate by signal, segfault etc.
+	if (WIFEXITED(status))
 		g_status = WEXITSTATUS(status);
-	if (WIFSIGNALED(status)) // returns true if child was terminated by signal
+	if (WIFSIGNALED(status))
 		g_status = 128 + status;
 }
 
