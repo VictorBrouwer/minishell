d0abe808acc9a47158890243883bad0470a3f3fe
Martin Haan <mhaan@f1r3s15.codam.nl>
2023-08-02 17:37:30 +0200

    refactored echo and unset

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -2,43 +2,88 @@
 #include "shell.h"
 #include <sys/errno.h>
 
-static int check_flag(char *arg);
+static int	skip_flags(char ***args);
+static int	print_args(char **args, int flag);
+static int	check_flag(char *arg);
 
 /*TODO: echo "test"test"test"  vs echo dit is een test */
 int builtin_echo(char **args)
 {
-	int flag;
+	int		flag;
+	char	**tmp;
 
 	if (!args)
 		return (ERROR);
-	args++;
-	flag = check_flag(*args);
+	tmp = args;
+	tmp++;
+	flag = skip_flags(&tmp);
+	/* flag = check_flag(*args); */
+	/* while (*args && flag) */
+	/* { */
+	/* 	flag = check_flag(*args); */
+	/* 	if (flag) */
+	/* 		args++; */
+	/* 	else */
+	/* 	{ */
+	/* 		flag = 1; */
+	/* 		break; */
+	/* 	} */
+	/* } */
+	if (print_args(tmp, flag) == 1)
+		return (1);
+	return (0);
+	/* while (*args) */
+	/* { */
+	/* 	if (ft_putstr_fd_protected(*args, STDOUT_FILENO, 0) == -1) */
+	/* 		return (ERROR); */
+	/* 	if (*(args + 1)) */
+	/* 	{ */
+	/* 		if (ft_putstr_fd_protected(" ", STDOUT_FILENO, 0) == -1) */
+	/* 			return (ERROR); */
+	/* 	} */
+	/* 	args++; */
+	/* } */
+	/* if (ft_putstr_fd_protected("", STDOUT_FILENO, flag < 1) == -1) */
+	/* 	return (ERROR); */
+}
+
+static int	skip_flags(char ***args)
+{
+	int flag;
+
+	flag = check_flag(**args);
 	while (*args && flag)
 	{
-		flag = check_flag(*args);
+		flag = check_flag(**args);
 		if (flag)
-			args++;
+			(*args)++;
 		else
 		{
 			flag = 1;
 			break;
 		}
 	}
+	return (flag);
+}
+
+static int	print_args(char **args, int flag)
+{
 	while (*args)
 	{
 		if (ft_putstr_fd_protected(*args, STDOUT_FILENO, 0) == -1)
-			return (ERROR);
+			return (1);
 		if (*(args + 1))
 		{
 			if (ft_putstr_fd_protected(" ", STDOUT_FILENO, 0) == -1)
-				return (ERROR);
+				return (1);
 		}
 		args++;
 	}
 	if (ft_putstr_fd_protected("", STDOUT_FILENO, flag < 1) == -1)
-		return (ERROR);
+		return (1);
 	return (0);
 }
+
 
 static int check_flag(char *arg)
 {
