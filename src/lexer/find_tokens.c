9ecb6a284a9bf41d10030d9bcd85d492be37cbd6
Martin Haan <mhaan@f1r3s2.codam.nl>
2023-08-14 12:14:20 +0200

    fixed quote syntax error, exit with numeric, exit status and export with numeric in name

--- src/lexer/find_tokens.c
+++ src/lexer/find_tokens.c
@@ -13,22 +13,22 @@
 #include "shell.h"
 #include "libft.h"
 
-static size_t	backslash_case(const char *s, size_t end);
-static size_t	quotes_case(const char *s, size_t start, size_t end);
-static size_t	var_case(const char *s, size_t end);
-static size_t	white_space_case(const char *s, size_t end);
+static long long	backslash_case(const char *s, long long end);
+static long long	quotes_case(const char *s, long long start, long long end);
+static long long	var_case(const char *s, long long end);
+static long long	white_space_case(const char *s, long long end);
 
-size_t	find_next_tok(const char *s, size_t beg)
+long long	find_next_tok(const char *s, long long beg)
 {
-	size_t	end;
+	long long	end;
 
 	if (!s[beg])
 		return (beg);
 	end = beg + 1;
-	if (s[beg] == ' ' && s[end] == ' ')
-		return (white_space_case(s, end));
 	if (ft_strchr(SPEC_DELIMS, s[beg]) && s[beg + 1] && s[beg] == s[beg + 1])
 		return (beg + 2);
+	else if (s[beg] == ' ' && s[end] == ' ')
+		return (white_space_case(s, end));
 	else if (s[beg] == '\\')
 		return (backslash_case(s, end));
 	else if (ft_strchr("\"\'", s[beg]))
@@ -46,34 +46,34 @@ size_t	find_next_tok(const char *s, size_t beg)
 	return (end);
 }
 
-static size_t	white_space_case(const char *s, size_t end)
+static long long	white_space_case(const char *s, long long end)
 {
 	while (s[end] == ' ')
 		end++;
 	return (end);
 }
 
-static size_t	backslash_case(const char *s, size_t end)
+static long long	backslash_case(const char *s, long long end)
 {
 	if (s[end])
 		return (end + 1);
 	return (end);
 }
 
-static size_t	quotes_case(const char *s, size_t start, size_t end)
+static long long	quotes_case(const char *s, long long start, long long end)
 {
 	while (s[end])
 	{
-		if (s[end] == s[start] && s[end - 1] != '\\')
+		if (s[end] == s[start])
 			break ;
 		end++;
 	}
 	if (s[end])
 		return (end + 1);
-	return (end);
+	return (-1);
 }
 
-static size_t	var_case(const char *s, size_t end)
+static long long	var_case(const char *s, long long end)
 {
 	while (s[end] && s[end] != '?' && !(ft_strchr(TOK_DELIMS, s[end])))
 		end++;
