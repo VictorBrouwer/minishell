87097d80e6831bbcec947bcb81c8ad3ec3740a40
Victor Brouwer <vbrouwer@f1r3s19.codam.nl>
2023-08-21 14:11:28 +0200

    fixed compi error

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,14 +6,14 @@
 /*   By: vbrouwer <vbrouwer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 11:14:57 by vbrouwer          #+#    #+#             */
-/*   Updated: 2023/08/21 11:37:22 by vbrouwer         ###   ########.fr       */
+/*   Updated: 2023/08/21 14:10:35 by vbrouwer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "shell.h"
 
-static void	exit_numeric_args_required(char *arg, t_shell *shell);
+static void	exit_numeric_args_required(t_shell *shell);
 static void	exit_with_status(char *arg, t_shell *shell);
 
 int	builtin_exit(char **args, t_shell *shell)
@@ -27,7 +27,7 @@ int	builtin_exit(char **args, t_shell *shell)
 			if (ft_stris_x(args[2], ft_isdigit) || \
 			((args[2][0] == '-' || args[2][0] == '+') \
 			&& ft_stris_x(args[2] + 1, ft_isdigit)))
-				exit_numeric_args_required(args[2], shell);
+				exit_numeric_args_required(shell);
 			ft_putstr_fd_prot("exit: too many arguments\n", STDERR_FILENO, 0);
 			return (1);
 		}
@@ -36,7 +36,7 @@ int	builtin_exit(char **args, t_shell *shell)
 		&& ft_stris_x(args[1] + 1, ft_isdigit)))
 			exit_with_status(args[1], shell);
 		else
-			exit_numeric_args_required(args[1], shell);
+			exit_numeric_args_required(shell);
 	}
 	clean_shell(shell);
 	rl_clear_history();
@@ -57,7 +57,7 @@ static void	exit_with_status(char *arg, t_shell *shell)
 	exit(status);
 }
 
-static void	exit_numeric_args_required(char *arg, t_shell *shell)
+static void	exit_numeric_args_required(t_shell *shell)
 {
 	int	status;
 
